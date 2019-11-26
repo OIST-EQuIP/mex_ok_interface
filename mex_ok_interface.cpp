65ee26d0ea05682eb9a87275c7bf08bd0e31db90
Hiroki Takahashi <hiroki.t.hashi@gmail.com>
2019-11-26 13:47:51 +0900

    Adapted to the new API. A pointer to okCFrontPanel, xem is derived from a smart pointer devptr. Unless devptr is in the global scope, it is destroyed every time mex_ok_interface is called and returned. Therefore it is necessary to place devptr in the global scope in order to carry xem over to the next execution.

--- mex_ok_interface.cpp
+++ mex_ok_interface.cpp
@@ -5,9 +5,12 @@
 #include <string.h>
 #include "okFrontPanelDLL.h"
 
-okCUsbFrontPanel *xem = NULL;
+okCFrontPanel *xem = NULL;
+OpalKelly::FrontPanelDevices devices;
+OpalKelly::FrontPanelPtr devptr;
 bool isopen = false;
 bool file_exists(const char * filename);
+
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]){
     /* commands
@@ -28,23 +31,21 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]){
         std::string serial;
         std::string devID;
         
-        if (FALSE == okFrontPanelDLL_LoadLib(NULL)) {
-            printf("Loading dll failed.\n");
-            return;
-        }
-        xem = new okCUsbFrontPanel();
-        if (okCUsbFrontPanel::NoError != xem->OpenBySerial()) {
+        count =  devices.GetCount();
+        printf("Number of devices found: %d\n", count);
+        devptr = devices.Open();
+        xem = devptr.get();
+        if (!xem) {
             printf("Opening failed.\n");
-            delete xem;
             return;
         }
-        count =  xem->GetDeviceCount();
-        serial = xem->GetSerialNumber();
-        devID = xem->GetDeviceID();
-        
-        printf("Device count: %d\n", count);
-        printf("Serial number: %s\n", serial.c_str());
-        printf("Device ID: %s\n", devID.c_str());
+        okTDeviceInfo  m_devInfo;
+        xem->GetDeviceInfo(&m_devInfo);
+        printf("Found a device: %s\n", m_devInfo.productName);
+        printf("Device firmware version: %d.%d\n", m_devInfo.deviceMajorVersion, m_devInfo.deviceMinorVersion);
+        printf("Device serial number: %s\n", m_devInfo.serialNumber);
+        printf("Device device ID: %d\n", m_devInfo.productID);
+        xem->LoadDefaultPLLConfiguration();
         isopen = true;
         
     } else if (strcmpi(cmd, "configure") == 0){
@@ -53,12 +54,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]){
             printf("FPGA is not open!\n");
             return;
         }
+        if (!xem){
+            printf("Null pointer!\n");
+            return;
+        }
         char bitfile[100];
         mxGetString(prhs[1], bitfile, 100);
         // check if file exists
         if (file_exists(bitfile)){
             std::string filename(bitfile);
-            if (okCUsbFrontPanel::NoError != xem->ConfigureFPGA(filename)) {
+            if (okCFrontPanel::NoError != xem->ConfigureFPGA(filename)) {
                 printf("Loading failed.\n");
                 return;
             } else {
@@ -70,42 +75,61 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]){
         }
         
     } else if (strcmpi(cmd, "setWireIn") == 0 || strcmpi(cmd, "SWI") == 0){ 
-        
+        if (!isopen) {
+            printf("FPGA is not open!\n");
+            return;
+        }
         int ep = mxGetScalar(prhs[1]);
         int val = mxGetScalar(prhs[2]);
-        if (okCUsbFrontPanel::NoError != xem->SetWireInValue(ep, val)) {
+        if (okCFrontPanel::NoError != xem->SetWireInValue(ep, val)) {
             printf("Wrinting to ep %d failed!\n", ep);
             return;
         }
         
     } else if (strcmpi(cmd, "updateWireIns") == 0 || strcmpi(cmd, "UWI") == 0){
-        
+        if (!isopen) {
+            printf("FPGA is not open!\n");
+            return;
+        }
         xem->UpdateWireIns();
         
     } else if (strcmpi(cmd, "getWireOutValue") == 0 || strcmpi(cmd, "GWO") == 0){
-        
+        if (!isopen) {
+            printf("FPGA is not open!\n");
+            return;
+        }
         int ep = mxGetScalar(prhs[1]);
         int val = xem->GetWireOutValue(ep);
         plhs[0] = mxCreateDoubleScalar(val);
         
     } else if (strcmpi(cmd, "updateWireOuts") == 0 || strcmpi(cmd, "UWO") == 0){
-        
+        if (!isopen) {
+            printf("FPGA is not open!\n");
+            return;
+        }
         xem->UpdateWireOuts();
         
     } else if (strcmpi(cmd, "activateTriggerIn") == 0 || strcmpi(cmd, "ATI") == 0) {
-        
+        if (!isopen) {
+            printf("FPGA is not open!\n");
+            return;
+        }
         int ep = mxGetScalar(prhs[1]);
         int bit = mxGetScalar(prhs[2]);
         
-        if (okCUsbFrontPanel::NoError != xem->ActivateTriggerIn(ep, bit)) {
+        if (okCFrontPanel::NoError != xem->ActivateTriggerIn(ep, bit)) {
             printf("Activating trigger bit %d at ep %d failed!\n", bit, ep);
             return;
         }
     
     } else if (strcmpi(cmd, "close") == 0){
-        
+        if (!isopen) {
+            printf("FPGA is not open!\n");
+            return;
+        }
         printf("Closing FPGA.\n");
-        delete xem;
+        xem->Close();
+        isopen = false;
         
     } else {        
         printf("Unknown command: %s.\n", cmd);
